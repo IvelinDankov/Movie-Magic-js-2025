09f7e913b703eed9737faabb0893fd164c694fa8
Ivelin Dankov <you@example.com>
2025-05-21 09:48:41 +0300

    Refactor project structure and add new styles and images
    - Deleted home.html and integrated its content into Handlebars template.
    - Updated index.js to serve static files and set Handlebars engine options.
    - Added new CSS files for various pages including 404, about, all-posts, create-edit, details, register-login, responsive, and search.
    - Added images for movies and logos to the public directory.
    - Updated home.hbs to link to the new CSS and image paths.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import { engine } from "express-handlebars";
 
 const app = express();
 
-app.engine("hbs", engine());
+app.use("/static", express.static("src/public"));
+
+app.engine(
+  "hbs",
+  engine({
+    extname: "hbs",
+  })
+);
 app.set("view engine", "hbs");
 app.set("views", "./src/views");
 
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", { layout: false });
 });
 
 app.listen(5006, () => console.log(`Server is listening on port 5006...`));
