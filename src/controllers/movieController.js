9b5750520d4f3c67a98c3aed52b146af5435b313
Ivelin Dankov <you@example.com>
2025-05-21 12:33:54 +0300

    add details page hartcoded details

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -6,4 +6,10 @@ movieController.get("/create", (req, res) => {
   res.render("create");
 });
 
+movieController.get("/:id/details", (req, res) => {
+  const movieId = req.params.id;
+
+  res.render("details");
+});
+
 export default movieController;
