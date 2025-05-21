a857461482db11c99af893abf096ec1fdf8913fb
Ivelin Dankov <you@example.com>
2025-05-21 09:36:15 +0300

    Add initial setup for Handlebars and static assets
    - Created a new CSS file to import various stylesheets for the application.
    - Added multiple image assets for the application, including logos and error images.
    - Updated package-lock.json and package.json to include express-handlebars as a dependency.
    - Configured express to use Handlebars as the view engine and set the views directory.
    - Created a basic home view using Handlebars.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import express from "express";
+import { engine } from "express-handlebars";
 
 const app = express();
 
+app.engine("hbs", engine());
+app.set("view engine", "hbs");
+app.set("views", "./src/views");
+
 app.get("/", (req, res) => {
-  res.send("<h1>It's works!!!</h1>");
+  res.render("home");
 });
 
-app.listen(5006, () => `Server is listening on port 5006...`);
+app.listen(5006, () => console.log(`Server is listening on port 5006...`));
