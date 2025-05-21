709453bf4c3f5dcefd30e3e5555fd61a853f5244
Ivelin Dankov <you@example.com>
2025-05-21 12:24:35 +0300

    Remove details and search pages; update routes and main layout for consistency

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ import movieController from "./controllers/movieController.js";
 const routes = Router();
 
 routes.use(homeController);
-routes.use(movieController);
+routes.use("/movies", movieController);
 
 routes.use((req, res, next) => {
   res.status(404).render("404");
